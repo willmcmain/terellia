df161455c31560a9e654d4b404eef823276deaa0
Will McMain <willmcmain@gmail.com>
2016-06-28 23:11:06 -0400

    basic actions; debug

--- engine.js
+++ engine.js
@@ -29,7 +29,11 @@ Engine.load = function(page) {
     //var page = this._pages[page];
     var filename = "pages/" + page + ".page";
 
-    $.ajax(filename).done(function(data) {
+    $.ajax({
+        url: filename,
+        cache: false,
+    })
+    .done(function(data) {
         var el = $('#content');
         el.css("opacity", 0);
         el.html(Engine.process(data));
@@ -37,12 +41,13 @@ Engine.load = function(page) {
         /*if(typeof page['post_load'] === "function") {
             page['post_load'](Engine.state);
         }*/
+        Engine.debug();
     });
 }
 
 
 Engine.render_links = function(str) {
-    var regx = /\[\[(\w+)\|?(.*)\]\]/g;
+    var regx = /\[\[(\w+)\|?(.*?)\]\]/g;
     return str.replace(regx, function(match, target, text) {
         /*if(Engine._pages[target] === undefined) {
             throw Error("Target '" + target + "' does not exist!");
@@ -63,14 +68,36 @@ Engine.process = function(text) {
         breaks: true,
     });
 
-    text = _.template(text)({s: Engine.state});
+    text = _.template(text)({s: Engine.state, Engine: Engine});
     text = this.render_links(text);
     text = md.render(text);
     return text;
 }
 
 
+Engine.debug = function() {
+    var el = $('#debug');
+    if(el) {
+        el.html(render_list(Engine.state));
+    }
+}
+
+
 })();
 
+render_list = function(obj) {
+    if(typeof obj === "object") {
+        html = "<ul>";
+        for(var key in obj) {
+            html += "<li><strong>" + key + "</strong>:";
+            html += render_list(obj[key]);
+        }
+        html += "</ul>";
+        return html;
+    }
+    else {
+        return obj;
+    }
+}
 
 Page = Engine.add_page;
